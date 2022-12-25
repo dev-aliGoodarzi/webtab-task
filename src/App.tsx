5299afa356dbcac3c6385f74c16d0441e81d99c9
ALi Goudarzi <me74242@gmail.com>
2022-12-25 18:26:34 +0330

    clean Code

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,30 @@
 import React, { useState, useEffect } from "react";
 // React
 // Components
-import { Routes, Route } from "react-router-dom";
 import Home from "./Containers/Home/Home";
-import { I_Advertise } from "./Models/advertiseInterface";
-import { getAdsByVehicleTypeAndAdType } from "./util/getAdsByVehicleTypeAndAdType";
-import { adsDataFetchStatus } from "./Models/customTypes";
 import Ads from "./Pages/Ads/Ads";
 import Ad from "./Components/Ad/Ad";
 // Components
 // Models
+import { adsDataFetchStatus } from "./Models/customTypes";
+import { I_Advertise } from "./Models/advertiseInterface";
 // Models
 // Modules
+import { Routes, Route } from "react-router-dom";
+import { getAdsByVehicleTypeAndAdType } from "./util/getAdsByVehicleTypeAndAdType";
 // Modules
 
 const App = () => {
+  //***********************************STATES***************************************** */
   const [ads, setAds] = useState<I_Advertise[]>([]);
   const [adsDataFetchStatus, setAdsDataFetchStatus] =
     useState<adsDataFetchStatus>("Pending");
+
+  //***********************************STATES***************************************** */
+  //********************************************************************************** */
+  //********************************************************************************** */
+  //***********************************SideEffect Manage****************************** */
+  // for getting datas @ first render !
   useEffect(() => {
     if (adsDataFetchStatus === "Pending") {
       getAdsByVehicleTypeAndAdType(1, 1, setAds, setAdsDataFetchStatus);
@@ -28,6 +35,7 @@ const App = () => {
     }
   }, [adsDataFetchStatus]);
   return (
+    //***********************************SideEffect Manage****************************** */
     <>
       <Routes>
         <Route path="/" element={<Home />} />
