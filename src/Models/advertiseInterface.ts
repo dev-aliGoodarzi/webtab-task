230955efc081380b54c7b79ae0e025f50ae9f63f
M-r-hydra <me74242@gmail.com>
2022-12-25 11:31:43 +0330

    add ads Interface

---
+++ src/Models/advertiseInterface.ts
@@ -0,0 +1,33 @@
+export interface I_Advertise {
+  advertise_id: number;
+  image: string;
+  advertise_type_id: number;
+  lat_lon: string;
+  phone: string;
+  address: string;
+  discount_code?: string;
+  discount_value?: string;
+  description: string;
+  duration: number;
+  AdvertiseSocialMedias: AdvertiseSocialMedia[];
+  favourite: boolean;
+  AdvertiseGalleries?: AdvertiseGallery[];
+}
+
+interface AdvertiseGallery {
+  path: string;
+  url_path: string;
+}
+
+interface AdvertiseSocialMedia {
+  social_media_id: string;
+  value?: string | string;
+  social_media: Socialmedia;
+}
+
+interface Socialmedia {
+  id: number;
+  name: string;
+  type: string;
+  url?: string;
+}
