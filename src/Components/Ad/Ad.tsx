090358dec89539ffdd99e15680e20a4b9dc35ff8
ALi Goudarzi <me74242@gmail.com>
2022-12-27 03:25:00 +0330

    #

--- src/Components/Ad/Ad.tsx
+++ src/Components/Ad/Ad.tsx
@@ -12,8 +12,6 @@ import { getSingleAd } from "../../util/getSingleAd";
 import { adsDataFetchStatus } from "../../Models/customTypes";
 import BackBtn from "../../util/BackBtn/BackBtn";
 import PendingAnimation from "../PendingAnimation/PendingAnimation";
-import LikeBtn from "../../util/LikeBtn/LikeBtn";
-import { manageFavorites } from "../../util/manageFavorites";
 // Moduels
 
 const Ad = () => {
